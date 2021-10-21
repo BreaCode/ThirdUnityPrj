f5d3721bb3c39bec91548d6a86fd4fe26f80c1ad
BreaCode <oolalkoo@gmail.com>
2021-10-21 17:40:36 +0300

    Уровень2 + примитивное движение

--- Assets/Scripts/Junk/PlayerAlt.cs
+++ Assets/Scripts/Junk/PlayerAlt.cs
@@ -1,31 +1,25 @@
 using UnityEngine;
-namespace Core
+namespace JunkYard
 {
     public class Player : MonoBehaviour
     {
-        private Transform _cameraTransform;
         private Rigidbody _playerRigidBody;
-        private Transform _playerTransform;
         private Vector3 _direction;
         private float _speed = 10;
         private void Start()
         {
-            _cameraTransform = gameObject.transform.GetChild(1);
-            _playerRigidBody = gameObject.GetComponentInChildren<Rigidbody>();
-            _playerTransform = gameObject.transform.GetChild(0);
+            _playerRigidBody = gameObject.GetComponent<Rigidbody>();
         }
         void Update()
         {
             _direction.x = Input.GetAxis("Horizontal");
             _direction.z = Input.GetAxis("Vertical");
             Move();
-            Debug.Log(_playerTransform.position);
         }
 
         private void Move()
         {
             _playerRigidBody.AddForce(_direction * _speed);
-            _cameraTransform.position = new Vector3(_playerTransform.position.x, 7, _playerTransform.position.z);
         }
     }
 }
--- Assets/Scripts/Starter.cs
+++ Assets/Scripts/Starter.cs
@@ -1,98 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Core
+public class Starter : MonoBehaviour
 {
-    public class Starter : MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
-        #region Fields
-        private GameObject _fixedCamera;
-        private GameObject _fromAboveCamera;
-        private GameObject _player;
-        private Component _playerRigidBody;
-        private GameObject _blueKey;
-        private GameObject _redKey;
-        private GameObject _yellowKey;
-        private GameObject _blueDoor;
-        private GameObject _redDoor;
-        private GameObject _yellowDoor;
-        #endregion
+        
+    }
 
-        #region Properties
-        public GameObject FixedCamera
-        {
-            get { return _fixedCamera; }
-        }
-        public GameObject FromAboveCamera
-        {
-            get { return _fromAboveCamera; }
-        }
-        public GameObject PlayerObject
-        {
-            get { return _player; }
-        }
-        public Component PlayerRigidBody
-        {
-            get { return _playerRigidBody; }
-        }
-        public GameObject BlueKey
-        {
-            get { return _blueKey; }
-        }
-        public GameObject RedKey
-        {
-            get { return _redKey; }
-        }
-        public GameObject YellowKey
-        {
-            get { return _yellowKey; }
-        }
-        public GameObject BlueDoor
-        {
-            get { return _blueDoor; }
-        }
-        public GameObject RedDoor
-        {
-            get { return _redDoor; }
-        }
-        public GameObject YellowDoor
-        {
-            get { return _yellowDoor; }
-        }
-        #endregion
-
-        void Awake()
-        {
-            GameObject[] findObject = GameObject.FindGameObjectsWithTag("MainCamera");
-            if (findObject[0] != null && findObject[1] != null)
-            {
-                _fixedCamera = findObject[0];
-                _fromAboveCamera = findObject[1];
-            }
-            else
-            {
-                Debug.Log("Camera not found");
-            }
-            _fixedCamera.SetActive(false);
-            GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
-            if (player[0] != null)
-            {
-                _player = player[0];
-                _playerRigidBody = _player.GetComponent<Rigidbody>();
-            }
-            else
-            {
-                Debug.Log("Player not found");
-            }
-            _blueKey = GameObject.Find("BlueKey");
-            _blueDoor = GameObject.Find("BlueDoor");
-            _redKey = GameObject.Find("RedKey");
-            _redDoor = GameObject.Find("RedDoor");
-            _yellowKey = GameObject.Find("YellowKey");
-            _yellowDoor = GameObject.Find("YellowDoor");
-
-            //Debug.Log(_fixedCamera.name + " " + _fromAboveCamera.name);
-            //Debug.Log(_player.name)
-        }
-
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
