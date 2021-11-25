e273e54b0075c944af6764eb4ed707941cfed0fb
BreaCode <oolalkoo@gmail.com>
2021-11-25 18:46:10 +0300

    Extention

---
+++ Assets/Scripts/Extention.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extention
+{
+
+    static int StirngLength(string inputString)
+    {
+        int output = 0;
+        output = inputString.Length;
+        return output;
+    }
+}
